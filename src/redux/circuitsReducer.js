abd9e137a422b0d9f05599aff63648dc80a52462
Mike <red.bymike@gmail.com>
2020-09-14 23:06:36 +0300

    circuitsReducer

--- src/redux/circuitsReducer.js
+++ src/redux/circuitsReducer.js
@@ -1,12 +1,47 @@
 import {ergastApi} from "../Api/api";
 
-const initialState = {
-    dataSource : [],
-    columns : [],
+const SETCIRCUITS="SETCIRCUITS";
+
+const initialState={
+    dataSource: [],
+    columns: [{
+        title: 'Circuit',
+        dataIndex: 'circuit',
+        key: 'circuit',
+    },
+        {
+            title: 'Locality',
+            dataIndex: 'locality',
+            key: 'locality',
+        },
+        {
+            title: 'Country',
+            dataIndex: 'country',
+            key: 'country',
+        },],
 }
 
-const circuitsReducer = (state = initialState, action) =>{
+const circuitsReducer=(state=initialState, action)=>{
+    switch (action.type) {
+        case SETCIRCUITS :
+            return {
+                ...state,
+                dataSource: action.circuitsData,
+            }
+        default :
+            return state;
+    }
+}
 
+export const setCircuitsData=(circuitsData)=>{
+    return {
+        type: SETCIRCUITS,
+        circuitsData,
+    }
+}
+
+export const circuitsData=()=>(dispatch)=>{
+    console.log(ergastApi.getCircuits());
 }
 
 export default circuitsReducer();
