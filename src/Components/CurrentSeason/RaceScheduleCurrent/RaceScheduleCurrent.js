ecfd3c38e6e9ddadf591b0c99dfff1a2cfa01ffc
Mike <red.bymike@gmail.com>
2020-10-20 23:16:30 +0300

    "next Race" in RaceScheduleCurrent Component

--- src/Components/CurrentSeason/RaceScheduleCurrent/RaceScheduleCurrent.js
+++ src/Components/CurrentSeason/RaceScheduleCurrent/RaceScheduleCurrent.js
@@ -3,8 +3,19 @@ import {Table} from 'antd';
 import 'antd/dist/antd.css';
 
 const RaceScheduleCurrent=(props)=>{
+
+
+    let today = new Date();
+    let currentDate = `${today.getFullYear()}${today.getMonth() + 1}${today.getDate()}`;
+    let filterRace = props.dataSource.map(race => +race.date.split("-").join("")).filter((el) => el >= +currentDate);
+    let nextRace = props.dataSource.filter(el => +el.date.split("-").join("") === filterRace[0]);
+
     return <div>
         <h1 className="h1_main_content">Календарь сезона 2020</h1>
+        <div className="next-race">
+            <p>Следующая гонка сезона:</p>
+            <p>{nextRace.length !== 0 ? nextRace[0].raceName : ""}</p>
+        </div>
         <div className="table_container">
             <Table width="100px" dataSource={props.dataSource} columns={props.columns} pagination={{
                 total: props.dataSource.length,
