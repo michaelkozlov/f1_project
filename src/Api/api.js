bb8df219a238cea55e1f5978a207fc0c3bb8cdfd
Michael <red.bymike@gmail.com>
2020-10-24 17:44:52 +0300

    firebase init

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,40 +1,40 @@
 import * as axios from "axios";
 
-const instanceErgast=axios.create({
-    baseURL: "http://ergast.com/api/f1/",
+const instanceErgast = axios.create({
+    baseURL: "https://ergast.com/api/f1/",
 });
 
-const instanceNewsApi=axios.create({
-    baseURL: "http://newsapi.org/v2/",
+const instanceNewsApi = axios.create({
+    baseURL: "https://newsapi.org/v2/",
 
 });
 
-const key="0212c0ec2f1f4f48a0aa80f89e58cf80";
+const key = "0212c0ec2f1f4f48a0aa80f89e58cf80";
 
-export const ergastApi={
+export const ergastApi = {
     getCircuits() {
         return instanceErgast.get(`/circuits.json`)
-            .then(response=>response.data);
+            .then(response => response.data);
     },
     getRaceScheduleCurrentSeason() {
         return instanceErgast.get(`/current.json`)
-            .then(response=>response.data);
+            .then(response => response.data);
     },
     getRaceSchedule(year) {
         return instanceErgast.get(`/${year}.json`)
-            .then(response=>response.data);
+            .then(response => response.data);
     },
     getCurrentDriverStandings() {
-        return instanceErgast.get(`/current/driverStandings.json`).then(response=>response.data);
+        return instanceErgast.get(`/current/driverStandings.json`).then(response => response.data);
     },
     getCurrentConstructorStandings() {
-        return instanceErgast.get(`/current/constructorStandings.json`).then(response=>response.data);
+        return instanceErgast.get(`/current/constructorStandings.json`).then(response => response.data);
     },
 }
 
-export const newsApi={
+export const newsApi = {
     getPopularNews() {
         return instanceNewsApi.get(`everything?domains=f1news.ru&apiKey=${key}`)
-            .then(response=>response.data);
+            .then(response => response.data);
     },
 }
