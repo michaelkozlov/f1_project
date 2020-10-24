ca3d36276758881c62f0ca0091d4e8a1f873fabf
Michael <red.bymike@gmail.com>
2020-10-24 16:31:27 +0300

    change h1 in News Component

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,26 +1,26 @@
 import React from "react";
 import style from "./News.module.css";
 
-const News=(props)=>{
+const News = (props) => {
     debugger;
     return <div>
-        <h1>News</h1>
+        <h1>Новости Формулы 1</h1>
         <div className={style.newsBlockWrapper}>
-        {props.news.map(el=>{
-            return (
-                <div className={style.newsBlock}>
-                    <div className={style.left}>
-                        <img src={`${el.urlToImage}`} alt=""/>
+            {props.news.map(el => {
+                return (
+                    <div className={style.newsBlock}>
+                        <div className={style.left}>
+                            <img src={`${el.urlToImage}`} alt="" />
+                        </div>
+                        <div className={style.right}>
+                            <h2>{el.title}</h2>
+                            <p>{el.description}</p>
+                            <p>{el.publishedAt}</p>
+                            <a href={`${el.url}`}>{el.url}</a>
+                        </div>
                     </div>
-                    <div className={style.right}>
-                        <h2>{el.title}</h2>
-                        <p>{el.description}</p>
-                        <p>{el.publishedAt}</p>
-                        <a href={`${el.url}`}>{el.url}</a>
-                    </div>
-                </div>
-            )
-        })}
+                )
+            })}
         </div>
     </div>
 }
